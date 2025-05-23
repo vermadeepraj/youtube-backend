4c62fe2e0379024a0229ddf75002bc653999ccf4
Deep Raj <2022umt1751@mnit.ac.in>
2025-05-24 02:45:59 +0530

    healthCheck controller added

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -4,7 +4,9 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const healthCheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthCheck response that simply returns the OK status as json with a message
+    return res
+        .status(200)
+        .json(200, null, "API is running and healthy")
 })
 
 export {
